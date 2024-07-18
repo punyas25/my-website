65f940cfaa990a5732e1233c7bc823a3f02cc0b1
Punyashree Shetty <punyashreeshetty@Punyashrees-MacBook-Air.local>
2024-07-18 17:15:08 +0530

    HEader & Footer components moved to specific folders

--- app/components/footer/Footer.js
+++ app/components/footer/Footer.js
@@ -1,4 +1,4 @@
-import logoImage from "../../public/images/tangerine.png"
+import logoImage from "../../../public/images/tangerine.png"
 import Image from 'next/image'
 import Link from 'next/link'
 
--- app/layout.js
+++ app/layout.js
@@ -1,7 +1,7 @@
 import "./globals.css";
 
-import Footer from "./components/Footer";
-import Header from "./components/Header";
+import Footer from "./components/footer/Footer";
+import Header from "./components/header/Header";
 
 export const metadata = {
   title: "Portfolio Website",
