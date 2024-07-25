e1dcc58a01cadf64f682202a1f2c68d2961a2bcc
Punyashree Shetty <punyashreeshetty@Punyashrees-MacBook-Air.local>
2024-07-25 22:02:47 +0530

    Fixes done for contact form styling

--- app/components/common/ContactForm.js
+++ app/components/common/ContactForm.js
@@ -1,72 +1,61 @@
 const ContactForm = () => {
   return (
-    <form className="w-3/6 my-10">
-      <div className="prose pb-12 ">
-        <h1 className="text-base text-gray-300">Contact</h1>
-        <p className="mt-1 text-sm leading-6 text-gray-300">
-          Please use the following form for inquires and feedback
-        </p>
-        <div className="mt-10 grid grid-cols-1 gap-x-6 gap-y-8 sm:grid-cols-6">
+    <form className="w-4/6 my-10">
+      <div className="pb-12">
+        <div className="prose">
+          <h1 className="text-gray-300 mb-2">Contact</h1>
+          <p className="mt-1 text-sm leading-6 text-gray-300">Please use the following form for inquires and feedback</p>
+        </div>
+
+        <div className="mt-6 grid grid-cols-1 gap-x-6 gap-y-8 sm:grid-cols-6">
           <div className="sm:col-span-3">
-            <div className="mt-2">
-              <div className="flex rounded-md shadow-sm ring ring-gray-300 focus:ring-pink-300 p-1">
-                <input
-                  id="full-name"
-                  name="full-name"
-                  type="text"
-                  autoComplete="full-name"
-                  placeholder="Full Name"
-                  // className="block w-full rounded-md border-0 py-1.5 text-gray-300 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6"
-                  className="block flex-1 bg-transparent py-1.5 pl-1 text-gray-300 placeholder:text-gray-400 sm:text-sm sm:leading-6"
-                />
-              </div>
+            <div className="mt-1">
+              <input
+                id="full-name"
+                name="full-name"
+                type="text"
+                autoComplete="full-name"
+                placeholder="Full Name"
+                className="block w-full rounded-md shadow-sm ring-1 bg-transparent p-2 text-gray-300 placeholder:text-gray-400 sm:text-sm sm:leading-6 ring-gray-300 focus:outline-none focus:ring-1 focus:ring-orange-700"
+              />
             </div>
           </div>
 
           <div className="sm:col-span-3">
-            <div className="mt-2">
-              {/* <div className="flex rounded-md shadow-sm ring-1 ring-inset ring-gray-300 focus-within:ring-2 focus-within:ring-inset focus-within:ring-pink sm:max-w-md"> */}
-              <div className="flex rounded-md shadow-sm ring-1 ring-gray-300 focus:ring-pink p-1">
-                <input
-                  id="email"
-                  name="email"
-                  type="email"
-                  placeholder="Email"
-                  autoComplete="email"
-                  className="block flex-1 border-0 bg-transparent py-1.5 pl-1 text-gray-300 placeholder:text-gray-400 sm:text-sm sm:leading-6"
-                />
-              </div>
+            <div className="mt-1">
+              <input
+                id="email"
+                name="email"
+                type="email"
+                placeholder="Email"
+                autoComplete="email"
+                className="block w-full rounded-md shadow-sm ring-1 bg-transparent p-2 text-gray-300 placeholder:text-gray-400 sm:text-sm sm:leading-6 ring-gray-300 focus:outline-none focus:ring-1 focus:ring-orange-700"
+              />
             </div>
           </div>
 
           <div className="col-span-full">
-            <div className="mt-2">
-              <div className="flex rounded-md shadow-sm ring-1 ring-gray-300 focus:ring-pink p-1">
-                <input
-                  id="subject"
-                  name="subject"
-                  type="text"
-                  placeholder="Subject"
-                  autoComplete="subject"
-                  // className="block w-full rounded-md border-0 py-1.5 text-gray-300 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6"
-                  className="block flex-1 border-0 bg-transparent py-1.5 pl-1 text-gray-300 placeholder:text-gray-400 sm:text-sm sm:leading-6"
-                />
-              </div>
+            <div className="mt-1">
+              <input
+                id="subject"
+                name="subject"
+                type="text"
+                placeholder="Subject"
+                autoComplete="subject"
+                className="block w-full rounded-md shadow-sm ring-1 bg-transparent p-2 text-gray-300 placeholder:text-gray-400 sm:text-sm sm:leading-6 ring-gray-300 focus:outline-none focus:ring-1 focus:ring-orange-700"
+              />
             </div>
           </div>
 
           <div className="col-span-full">
-            <div className="mt-2">
-              <div className="flex rounded-md shadow-sm ring-1 ring-gray-300 focus:ring-pink p-1">
-                <textarea
-                  id="about"
-                  name="about"
-                  rows={3}
-                  // className="block w-full rounded-md border-0 py-1.5 text-gray-300 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6"
-                  className="block flex-1 border-0 bg-transparent py-1.5 pl-1 text-gray-300 placeholder:text-gray-400 sm:text-sm sm:leading-6"
-                  placeholder="Message"
-                />
-              </div>
+            <div className="mt-1">
+              <textarea
+                id="message"
+                name="message"
+                rows={3}
+                className="block w-full rounded-md shadow-sm ring-1 bg-transparent p-2 text-gray-300 placeholder:text-gray-400 sm:text-sm sm:leading-6 ring-gray-300 focus:outline-none focus:ring-1 focus:ring-orange-700"
+                placeholder="Message"
+              />
             </div>
           </div>
         </div>
