eda85ea27db946c5dcd3d9b98bf40df47d85fb0c
Punyashree Shetty <punyashreeshetty@Punyashrees-MacBook-Air.local>
2024-07-26 14:28:19 +0530

    Page links added to Navbar components and props updated

--- app/components/header/Header.js
+++ app/components/header/Header.js
@@ -8,14 +8,23 @@ import Navbar from "./Navbar"
 const Header = () => {
   const [mobileMenuOpen, setMobileMenuOpen] = useState(false)
 
+  const socMedLinks = {
+    twitter: "add link here",
+    instagram: "https://www.instagram.com/doodlesbypost/",
+    facebook: "https://www.facebook.com/punyashree.shetty",
+    linkedin: "https://www.linkedin.com/in/punyashree-shetty-862029102/",
+  }
+
   return (
     <header className="bg-slate-900">
       <Navbar
         setMobileMenuOpen={setMobileMenuOpen}
+        socMedLinks={socMedLinks}
       />
       <MobileNavbar
         setMobileMenuOpen={setMobileMenuOpen}
         mobileMenuOpen={mobileMenuOpen}
+        socMedLinks={socMedLinks}
       />
     </header>
   )
