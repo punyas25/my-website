19a72e258b621e17a03948bbd8268e0f64e5c73b
Punyashree Shetty <punyashreeshetty@Punyashrees-MacBook-Air.local>
2024-07-26 12:32:40 +0530

    About me component

---
+++ app/components/common/AboutMe.js
@@ -0,0 +1,48 @@
+import Image from "next/image"
+
+import profileImg from "../../../public/images/profile.jpg"
+
+const AboutMe = () => {
+  return (
+    <div className="w-4/6 grid lg:grid-cols-2 grid-cols-1 gap-8 my-10">
+      <div className="img-col">
+        <Image
+          className="rounded-md w-full object-cover"
+          src={profileImg}
+          alt=""
+        />
+      </div>
+      <div className="prose info-col flex flex-col text-gray-300">
+        <h1 className="text-gray-200 mb-2">Hobby Artist from Mumbai & Web Developer.
+        </h1>
+        <p className="mb-2">
+          Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.
+        </p>
+        <div>
+          <ul>
+            <li>
+              Instagram: @doodlesbypost
+            </li>
+            <li>
+              Email: email@example.com
+            </li>
+            <li>
+              City: Mumbai, India
+            </li>
+            <li>
+              Degree: Master
+            </li>
+            <li>
+              LinkedIn: linkedin/link
+            </li>
+          </ul>
+        </div>
+        <div>
+          Officiis eligendi itaque labore et dolorum mollitia officiis optio vero. Quisquam sunt adipisci omnis et ut. Nulla accusantium dolor incidunt officia tempore. Et eius omnis. Cupiditate ut dicta maxime officiis quidem quia. Sed et consectetur qui quia repellendus itaque neque. Aliquid amet quidem ut quaerat cupiditate. Ab et eum qui repellendus omnis culpa magni laudantium dolores.
+        </div>
+      </div>
+    </div>
+  )
+}
+
+export default AboutMe
