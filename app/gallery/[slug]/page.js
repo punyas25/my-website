e3583c333db9ea9c88e5efcf59bb2648150d9ed4
Punyashree Shetty <punyashreeshetty@Punyashrees-MacBook-Air.local>
2024-08-01 12:40:44 +0530

    Api added to fetch project info and render it on the project page

--- app/gallery/[slug]/page.js
+++ app/gallery/[slug]/page.js
@@ -1,35 +1,44 @@
-import HeroBlock from "../../components/common/HeroBlock"
+"use client"
+
+import { useEffect, useState } from "react"
+
 import Carousel from "../components/Carousel"
 import ProjectInfo from "../components/ProjectInfo"
+import Loader from "../../components/common/Loader"
 
-export default function Page() {
-  const statistics = [
-    {
-      title: "Project Year",
-      info: "20xx",
-    },
-    {
-      title: "Project Type",
-      info: "Hobby Project",
-    },
-    {
-      title: "Postcards sent",
-      info: "52",
-    }
-  ]
+import { fetchProjectData } from "../api"
+
+export default function Page({ params }) {
+  const { slug } = params
+
+  const [projectData, setProjectData] = useState([])
+  const [carouselData, setCarouselData] = useState([])
+  const [isLoading, setIsLoading] = useState(true)
+
+  useEffect(() => {
+    fetchProjectData(slug)
+      .then(response => {
+        setProjectData(response[0])
+        setCarouselData(response[0].project_images)
+      })
+      .catch(error => {
+        console.log(error)
+      })
+      .finally(() => setIsLoading(false))
+  }, [])
+
+
+  if (isLoading) {
+    return <Loader />
+  }
+
   return (
     <div className="flex min-h-screen flex-col items-center justify-between bg-slate-800 text-gray-300 p-5">
-      <HeroBlock
-        title="Project page Quis eleifend quam adipiscing vitae proin sagittis."
-        content="Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat."
-        link="/"
-        linkText="Contact Me"
-      />
-      <Carousel />
-      <ProjectInfo
-        title="Project quis"
-        content="Autem ipsum nam porro corporis rerum. Quis eos dolorem eos itaque inventore commodi labore quia quia. Exercitationem repudiandae officiis neque suscipit non officia eaque itaque enim. Voluptatem officia accusantium nesciunt est omnis tempora consectetur dignissimos. Sequi nulla at esse enim cum deserunt eius. Amet consequatur qui dolore veniam voluptatem voluptatem sit. Non aspernatur atque natus ut cum nam et. Praesentium error dolores rerum minus sequi quia veritatis eum."
-        statistics={statistics}
+      <div className="prose hero-block flex flex-col w-3/6 my-20 mx-10 place-items-center text-center">
+        <h1 className="text-gray-300 mb-2">{projectData.project_name}</h1>
+      </div>
+      <Carousel carouselData={carouselData} />
+      <ProjectInfo projectData={projectData}
       />
     </div>
   );
